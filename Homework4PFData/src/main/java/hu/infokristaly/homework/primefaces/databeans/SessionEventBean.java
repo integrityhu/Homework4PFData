9a08369d655875adfb47ab79e70d2e16911a5415
Papp Zoltán <papp.zoltan@integrity.hu>
2016-03-20 22:26:36 +0100

    server info

--- Homework4PFData/src/main/java/hu/infokristaly/homework/primefaces/databeans/SessionEventBean.java
+++ Homework4PFData/src/main/java/hu/infokristaly/homework/primefaces/databeans/SessionEventBean.java
@@ -6,10 +6,22 @@ import java.util.Map;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
+import javax.faces.component.html.HtmlOutputText;
 import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.swing.ListModel;
 
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.as.controller.client.helpers.Operations;
+import org.jboss.dmr.ModelNode;
+import org.jboss.dmr.ModelType;
+import org.primefaces.component.column.Column;
+import org.primefaces.component.datatable.DataTable;
 import org.primefaces.event.RowEditEvent;
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
@@ -20,111 +32,197 @@ import hu.infokristaly.homework.primefaces.domain.SystemUser;
 @Named
 public class SessionEventBean implements Serializable {
 
-    private static final long serialVersionUID = 1889487896935257529L;
+	private static final long serialVersionUID = 1889487896935257529L;
 
-    @Inject
-    private StatelessDataBean dataBean;
+	@Inject
+	private StatelessDataBean dataBean;
 
-    private LazyDataModel<SystemUser> lazyDataModel;
+	private LazyDataModel<SystemUser> lazyDataModel;
 
-    private SystemUser[] selectedUsers = {};
-    
-    private SystemUser newSystemUser;
+	private SystemUser[] selectedUsers = {};
 
-    public LazyDataModel<SystemUser> getLazyDataModel() {
-        if (lazyDataModel == null) {
-            lazyDataModel = new LazyDataModel<SystemUser>() {
+	private SystemUser newSystemUser;
 
-                private static final long serialVersionUID = 1678907483750487431L;
+	public LazyDataModel<SystemUser> getLazyDataModel() {
+		if (lazyDataModel == null) {
+			lazyDataModel = new LazyDataModel<SystemUser>() {
 
-                private Map<String, Object> actualfilters;
+				private static final long serialVersionUID = 1678907483750487431L;
 
-                private String actualOrderField;
+				private Map<String, Object> actualfilters;
 
-                private SortOrder actualSortOrder;
+				private String actualOrderField;
 
-                @Override
-                public SystemUser getRowData(String rowKey) {
-                    SystemUser systemUser = new SystemUser();
-                    systemUser.setUserid(Long.valueOf(rowKey));
-                    return dataBean.find(systemUser);
-                }
+				private SortOrder actualSortOrder;
 
-                @Override
-                public Object getRowKey(SystemUser systemUser) {
-                    return systemUser.getUserid();
-                }
+				private Integer count;
 
-                @Override
-                public List<SystemUser> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
-                    this.setPageSize(pageSize);
-                    this.actualfilters = filters;
-                    if (sortField != null) {
-                        this.actualOrderField = sortField;
-                    }
-                    if (sortOrder != null) {
-                        this.actualSortOrder = sortOrder;
-                    }
+				@Override
+				public SystemUser getRowData(String rowKey) {
+					SystemUser systemUser = new SystemUser();
+					systemUser.setUserid(Long.valueOf(rowKey));
+					return dataBean.find(systemUser);
+				}
 
-                    List<SystemUser> result = (List<SystemUser>) dataBean.findRange(first, pageSize, actualOrderField, actualSortOrder, filters);
-                    return result;
-                }
+				@Override
+				public Object getRowKey(SystemUser systemUser) {
+					return systemUser.getUserid();
+				}
 
-                @Override
-                public int getRowCount() {
-                    int result = dataBean.count(actualfilters);
-                    return result;
-                }
+				@Override
+				public List<SystemUser> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+					this.count = null;
+					this.setPageSize(pageSize);
+					this.actualfilters = filters;
+					if (sortField != null) {
+						this.actualOrderField = sortField;
+					}
+					if (sortOrder != null) {
+						this.actualSortOrder = sortOrder;
+					}
 
-            };
-        }
-        return lazyDataModel;
-    }
-    
-    public List<SystemUser> findAll() {
-    	return dataBean.findAll();
-    }
+					List<SystemUser> result = (List<SystemUser>) dataBean.findRange(first, pageSize, actualOrderField, actualSortOrder, filters);
+					return result;
+				}
 
-    public void createNewSystemUser() {
-        setNewSystemUser(new SystemUser());
-    }
-    
-    public void onEdit(RowEditEvent event) {
-        SystemUser systemUser = (SystemUser) event.getObject();
-        FacesMessage msg = new FacesMessage("Kliens adatai átszerkesztve", systemUser.getUsername());
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
+				@Override
+				public int getRowCount() {
+					if (count == null) {
+						count = dataBean.count(actualfilters);
+					}
+					return count;
+				}
 
-    public void onCancel(RowEditEvent event) {
-        FacesMessage msg = new FacesMessage("Kliens módosítása visszavonva", ((SystemUser) event.getObject()).getUsername());
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
+			};
+		}
+		return lazyDataModel;
+	}
 
-    public SystemUser[] getSelectedUsers() {
-        return selectedUsers;
-    }
+	public List<SystemUser> findAll() {
+		return dataBean.findAll();
+	}
 
-    public void setSelectedUsers(SystemUser[] selectedUsers) {
-        this.selectedUsers = selectedUsers;
-    }
+	public void createNewSystemUser() {
+		setNewSystemUser(new SystemUser());
+	}
 
-    public SystemUser getNewSystemUser() {
-        return newSystemUser;
-    }
+	public void onEdit(RowEditEvent event) {
+		SystemUser systemUser = (SystemUser) event.getObject();
+		FacesMessage msg = new FacesMessage("Kliens adatai átszerkesztve", systemUser.getUsername());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
 
-    public void setNewSystemUser(SystemUser newSystemUser) {
-        this.newSystemUser = newSystemUser;
-    }
-    
-    public void disableUsers() {
-        dataBean.disableUsers(selectedUsers);
-    }
-    
-    public void persistCurrent() {
-        dataBean.persist(newSystemUser);
-    }
-    
-    public void deleteUsers() {
-        dataBean.deleteUsers(selectedUsers);
-    }
-}
+	public void onCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Kliens módosítása visszavonva", ((SystemUser) event.getObject()).getUsername());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public SystemUser[] getSelectedUsers() {
+		return selectedUsers;
+	}
+
+	public void setSelectedUsers(SystemUser[] selectedUsers) {
+		this.selectedUsers = selectedUsers;
+	}
+
+	public SystemUser getNewSystemUser() {
+		return newSystemUser;
+	}
+
+	public void setNewSystemUser(SystemUser newSystemUser) {
+		this.newSystemUser = newSystemUser;
+	}
+
+	public void disableUsers() {
+		dataBean.disableUsers(selectedUsers);
+	}
+
+	public void persistCurrent() {
+		dataBean.persist(newSystemUser);
+	}
+
+	public void deleteUsers() {
+		dataBean.deleteUsers(selectedUsers);
+	}
+
+	public void phaseListener(PhaseEvent e) {
+		if (e.getPhaseId().equals(PhaseId.RENDER_RESPONSE)) {
+			FacesContext fc = FacesContext.getCurrentInstance();
+			UIViewRoot root = fc.getViewRoot();
+			DataTable table = (DataTable) root.findComponent(":lazyForm:lazyTable");
+			if (table != null) {
+				table.getChildren().forEach(c -> {
+					if ((c instanceof Column) && (c.getChildren() != null) && (c.getChildren().size() > 0)) {
+						UIComponent content = c.getChildren().get(0);
+						if (content instanceof HtmlOutputText) {
+							System.out.println(content.getValueExpression("value"));
+						}
+					}
+				});
+			}
+		}
+	}
+
+	public void getDataConnectionInfo() {
+		StringBuffer info = dataBean.getEntityConnectionInfo();
+		FacesMessage msg = new FacesMessage("Database info", info.toString());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void getAppServerInfo() throws Exception {
+		try (ModelControllerClient client = ModelControllerClient.Factory.create("localhost", 9990)) {
+			ModelNode op = Operations.createOperation("product-info", new ModelNode().setEmptyList());			  
+			ModelNode result = client.execute(op);
+			boolean found = Operations.isSuccessfulOutcome(result);
+			if (!found) {
+				op = Operations.createReadResourceOperation(new ModelNode().setEmptyList());
+				result = client.execute(op);
+				found = Operations.isSuccessfulOutcome(result); 
+			}
+			if (found) {
+				final ModelNode model = Operations.readResult(result);
+				String productName = null;
+				String productVersion = null;
+				if (model.getType().equals(ModelType.LIST)) {
+					final ModelNode model0 = model.get(0);
+					if (model0.getType().equals(ModelType.OBJECT)) {
+						final ModelNode summary = model0.get("summary");
+						
+						if (summary.hasDefined("product-name")) {
+							productName = summary.get("product-name").asString();
+						} else {
+							productName = "WildFly";
+						}
+
+						
+						if (summary.hasDefined("product-version")) {
+							productVersion = summary.get("product-version").asString();
+						}
+						
+					}
+				} else {
+					if (model.hasDefined("product-name")) {
+						productName = model.get("product-name").asString();
+					}
+					if (model.hasDefined("product-version")) {
+						productVersion = model.get("product-version").asString();
+					}				
+				}
+				StringBuffer info = new StringBuffer();
+				info.append("Product: " + productName + "\n");
+				info.append("Product Version: " + productVersion);
+				
+				FacesMessage msg = new FacesMessage("AppServer info", info.toString());
+				FacesContext.getCurrentInstance().addMessage(null, msg);
+			} else {
+				System.out.println(result);
+				System.out.println(Operations.getFailureDescription(result));
+			}
+		}catch(
+
+	Exception ex)
+
+	{
+		System.out.println(ex);
+	}
+}}
--- Homework4PFData/src/main/java/hu/infokristaly/homework/primefaces/databeans/StatelessDataBean.java
+++ Homework4PFData/src/main/java/hu/infokristaly/homework/primefaces/databeans/StatelessDataBean.java
@@ -1,14 +1,21 @@
 package hu.infokristaly.homework.primefaces.databeans;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
 
 import org.primefaces.model.SortOrder;
 
@@ -18,45 +25,89 @@ import hu.infokristaly.homework.primefaces.domain.SystemUser;
 @Named
 public class StatelessDataBean implements Serializable {
 
-    private static final long serialVersionUID = 1889487896935257529L;
+	private static final long serialVersionUID = 1889487896935257529L;
 
-    @Inject
-    private EntityManager em;
-    
-    @SuppressWarnings("unchecked")
-    public List<SystemUser> findRange(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
-        Query q = em.createQuery("from SystemUser");
-        q.setFirstResult(first);
-        q.setMaxResults(pageSize);
-        return q.getResultList();
-    }
+	@Inject
+	private EntityManager em;
 
-    public SystemUser find(SystemUser systemUser) {
-        return em.find(SystemUser.class, systemUser.getUserid());
-    }
+	@SuppressWarnings("unchecked")
+	public List<SystemUser> findRange(int first, int pageSize, String sortField, SortOrder sortOrder,
+			Map<String, Object> filters) {
+		StringBuffer qStr = new StringBuffer("from SystemUser");
+		if ((filters != null) && filters.containsKey("username")) {			
+			qStr.append(" where username like :username");
+		}
+		Query q = em.createQuery(qStr.toString());
+		if ((filters != null) && filters.containsKey("username")) {
+			String name = (String)filters.get("username");
+			q.setParameter("username", name+"%");
+		}
+		q.setFirstResult(first);
+		q.setMaxResults(pageSize);
+		return q.getResultList();
+	}
 
-    public int count(Map<String, Object> actualfilters) {
-        Query q = em.createQuery("select count(*) from SystemUser");
-        return ((Long)q.getSingleResult()).intValue();
-    }
+	public SystemUser find(SystemUser systemUser) {
+		return em.find(SystemUser.class, systemUser.getUserid());
+	}
 
-    public void disableUsers(SystemUser[] selectedUsers) {
-        // TODO Auto-generated method stub
-        
-    }
+	public int count(Map<String, Object> actualfilters) {
+		StringBuffer qStr = new StringBuffer("select count(*) from SystemUser");
+		
+		if ((actualfilters != null) && actualfilters.containsKey("username")) {
+			qStr.append(" where username like :username");
+		}
+		Query q = em.createQuery(qStr.toString());
+		if ((actualfilters != null) && actualfilters.containsKey("username")) {
+			String name = (String)actualfilters.get("username");			
+			q.setParameter("username", name+"%");
+			System.out.println("username : " + name);
+		}
+		return ((Long) q.getSingleResult()).intValue();
+	}
 
-    public void persist(SystemUser newSystemUser) {        
-        em.persist(newSystemUser);
-    }
+	public void disableUsers(SystemUser[] selectedUsers) {
+		// TODO Auto-generated method stub
 
-    public void deleteUsers(SystemUser[] selectedUsers) {
-        // TODO Auto-generated method stub
-        
-    }
+	}
+
+	public void persist(SystemUser newSystemUser) {
+		em.persist(newSystemUser);
+	}
+
+	public void deleteUsers(SystemUser[] selectedUsers) {
+		// TODO Auto-generated method stub
+
+	}
 
 	public List<SystemUser> findAll() {
-		Query q =  em.createQuery("from SystemUser");
-		return q.getResultList(); 
+		Query q = em.createQuery("from SystemUser");
+		return q.getResultList();
 	}
-    
+
+    public StringBuffer getEntityConnectionInfo() {
+    	StringBuffer info = new StringBuffer();
+        Session session = (Session) em.getDelegate();
+        SessionFactoryImplementor sfi = (SessionFactoryImplementor) session.getSessionFactory();        
+        org.hibernate.engine.spi.SessionImplementor sessionImp = (org.hibernate.engine.spi.SessionImplementor) em.getDelegate();
+        Connection connection = sessionImp.connection();
+        try {         
+            String dataBaseUrl = connection.getMetaData().getURL();
+            String dataBaseProductName = connection.getMetaData().getDatabaseProductName();
+            String dataBaseProductVersion = connection.getMetaData().getDatabaseProductVersion();
+            String driverName = connection.getMetaData().getDriverName();
+            String driverVersion = connection.getMetaData().getDriverVersion();
+            int major = connection.getMetaData().getDatabaseMajorVersion();
+            int minor = connection.getMetaData().getDatabaseMinorVersion();
+            
+            info.append("BaseUrl: "+dataBaseUrl+"\n");            
+            info.append("BaseProductName: "+dataBaseProductName + " " + dataBaseProductVersion + "("+ major + "." + minor+")\n");
+            info.append("DriverName: "+driverName+"\n");
+            info.append("DriverVersion: "+driverVersion);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return info;
+    }
+
 }
